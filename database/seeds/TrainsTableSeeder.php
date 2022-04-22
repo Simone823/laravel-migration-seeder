ab5bd97ee00e9d234f1b28d7c4a798f24b90b72e
Simone823 <simone.daglio823@outlook.it>
2022-04-23 01:29:03 +0200

    use train, new train, set properties trains seeder

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Train;
 use Illuminate\Database\Seeder;
 
 // Import faker php
@@ -12,8 +13,25 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        
+        // Ciclo per creare 20 volte un nuovo treno
+        for ($i=0; $i < 20; $i++) { 
+            
+            // Creo un nuovo treno
+            $train = new Train ();
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->time();
+            $train->arrival_time = $faker->time();
+            $train->train_code = $faker->randomNumber(5, true);
+            $train->number_of_carriages = $faker->randomDigitNotNull();
+            $train->on_time = $faker->boolean();
+            $train->deleted = $faker->boolean();
+            $train->save();
+        }
+
     }
 }
