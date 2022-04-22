f3247b089b4a1453c0778b97104f383ad3de6cc4
Simone823 <simone.daglio823@outlook.it>
2022-04-22 22:26:18 +0200

    creazione trains seeder, import fakerphp

---
+++ database/seeds/TrainsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+// Import faker php
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
